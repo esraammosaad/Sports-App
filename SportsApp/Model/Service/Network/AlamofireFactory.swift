960803700eff234b31ee53b4c6988e37a17ae7d9
arwaKhaled23 <khaledarwa14@gmail.com>
2025-05-20 04:50:50 +0300

    testing alamofire factory and part of sport service

--- SportsApp/Model/Service/Network/AlamofireFactory.swift
+++ SportsApp/Model/Service/Network/AlamofireFactory.swift
@@ -12,17 +12,17 @@ class AlamofireFactory{
     
     
     static func leagueRequest(url : String, completion : @escaping (LeagueResponse?) -> Void){
-        AF.request(url).validate(statusCode: 200..<300).responseDecodable(of:LeagueResponse.self){
-            response in
-            switch response.result{
-            case .failure(let error):
-                print(error.localizedDescription)
-                completion(nil)
-            case .success(let successResponse):
-                completion(successResponse)
-            }
-        }
-    }
+         AF.request(url).validate(statusCode: 200..<300).responseDecodable(of:LeagueResponse.self){
+             response in
+             switch response.result{
+             case .failure(let error):
+                 print(error.localizedDescription)
+                 completion(nil)
+             case .success(let successResponse):
+                 completion(successResponse)
+             }
+         }
+     }
     
     static func leagueDetailsRequest(url : String, completion : @escaping (EventResponse?) -> Void){
         AF.request(url).validate(statusCode: 200..<300).responseDecodable(of:EventResponse.self){
@@ -51,7 +51,6 @@ class AlamofireFactory{
     }
     
     
-    //https://apiv2.allsportsapi.com/tennis/?met=Leagues&APIkey=edaabe5e8bfd4f780d114f7cafd181e0acc496f308f7685089b5cd9ecf224f5d&countryId=267
     
     static func tennisLeagueDetails(url : String, completion : @escaping (TeamResponse?) -> Void){
         AF.request(url).validate(statusCode: 200..<300).responseDecodable(of:TeamResponse.self){
@@ -83,6 +82,7 @@ class AlamofireFactory{
     static func  getTennisDetails(url : String, completion : @escaping (TennisEventResponse?) -> Void){
         AF.request(url).validate(statusCode: 200..<300).responseDecodable(of:TennisEventResponse.self){
             response in
+            print(url)
             switch response.result{
             case .failure(let error):
                 print(error.localizedDescription)
@@ -92,11 +92,5 @@ class AlamofireFactory{
             }
         }
     }
-    
-    
-    
-    
-    
-    
-    
+     
 }
